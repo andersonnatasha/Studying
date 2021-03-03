209651498cdd1fa29cb911bff8ea71649901853b
andersonnatasha <andersonnatashaa@gmail.com>
2021-03-03 11:25:43 -0800

    secondary_diagonal

--- questions.py
+++ questions.py
@@ -58,7 +58,7 @@ def sort_diagonally(matrix):
     return matrix
 
 
-sort_diagonally([[1, 4, -2], [-2, 3, 4], [3, 1, 3]])
+# sort_diagonally([[1, 4, -2], [-2, 3, 4], [3, 1, 3]])
 
 
 def maximalPalindrome(s):
@@ -97,7 +97,7 @@ def maximalPalindrome(s):
             evens.append((element[0], element[1] - 1))
 
     evens = sorted(evens, key=lambda i: i[0])
-    print(evens)
+    # print(evens)
 
     for element in evens:
         for i in range(element[1]//2):
@@ -107,4 +107,54 @@ def maximalPalindrome(s):
     return "".join(begining + middle + end)
 
 
-print(maximalPalindrome("aaabbbcc"))
+maximalPalindrome("aaabbbcc")
+
+
+def secondary_diagonal(matrix):
+    """Given a square matrix of positive integers, sort thr number in each of its diagonals parallel to the secondary diagonal.
+    Each diagonal should contain the same set of numbers as before but sorted in ascending order from the bottom-left to top-right"""
+    def get_next_diag_point(coordinates, max_row, max_column):
+        # if i can go down do that
+        if coordinates[0] < max_row:
+            return (coordinates[0] + 1, coordinates[1])
+        # elif if can go right do that
+        if coordinates[1] < max_column:
+            return(coordinates[0], coordinates[1] + 1)
+        # else return none
+        else:
+            return (None, None)
+
+    def sort_diag(matrix, diag_coordinates, max_row, max_column):
+
+        current_row = diag_coordinates[0]
+        current_column = diag_coordinates[1]
+
+        diag_values = []
+
+        while 0 <= current_row <= max_row and 0 <= current_column <= max_row:
+            diag_values.append(matrix[current_row][current_column])
+            current_row -= 1
+            current_column += 1
+
+        current_row = diag_coordinates[0]
+        current_column = diag_coordinates[1]
+
+        while 0 <= current_row <= max_row and 0 <= current_column <= max_row:
+            diag_values = sorted(diag_values)
+            matrix[current_row][current_column] = diag_values.pop(0)
+            current_row -= 1
+            current_column += 1
+
+        diag_coordinates = get_next_diag_point(
+            diag_coordinates, max_row, max_column)
+
+        if diag_coordinates[0] != None:
+            return sort_diag(matrix, diag_coordinates, max_row, max_column)
+
+    sort_diag(matrix, (0, 0), len(matrix) - 1, len(matrix[0]) - 1)
+
+    return matrix
+
+
+print(secondary_diagonal(
+    [[1, 3, 9, 4], [9, 5, 7, 7], [3, 6, 9, 7], [1, 2, 2, 2]]))
